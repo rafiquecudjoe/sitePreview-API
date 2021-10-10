2f672249de5be3fe5b0459467e11b73cbd50040f
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2021-10-10 20:40:39 +0000

    added Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule,DocumentBuilder } from '@nestjs/swagger';
 import {PrismaService} from './prisma.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Site Preview example')
+    .setDescription('Site Preview API description')
+    .setVersion('1.0')
+    .build();
+  
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/', app, document);
+  
+  
+  const prismaservice: PrismaService = app.get(PrismaService)
+  prismaservice.enableShutdownHooks(app)
   await app.listen(4000);
 }
 bootstrap();
